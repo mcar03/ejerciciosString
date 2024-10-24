80806b41002e661fd27abb344d4485101aa6742b
mcar03 <mcar03.474@gmail.com>
2024-10-25 00:29:53 +0200

    v

---
+++ ejercicios_desestructuración/ejercicios.js
@@ -0,0 +1,31 @@
+/*const [dia,mes,anno] = ["06","Octubre", "2024"]
+console.log(dia + " " + mes + " " + anno)*/
+
+/*const [uno, ,dos, , tres, ] =  [1,2,3,4,5,6]
+console.log(uno, dos, tres)*/
+
+/*const [uno, dos, ...demas] = [1,2,3,4,5,6]
+console.log(uno + " " + dos + " " + demas) */
+
+/*const person = { nombre: 'Luis', apellidos: 'Molina', telefono: '+34666554433' }
+const { nombre, apellidos, telefono } = person;
+console.log(person.nombre + " " + person.apellidos + " " + person.telefono)*/
+
+/*const person = { nombre: 'Luis', apellidos: 'Molina', telefono: '+34666554433' }
+for (const [key, value] of Object.entries(person)) {
+     console.log(key, value);
+    }
+*/
+
+/*const [{ x:x1, y:y1 } ,{ x:x2 ,y:y2}] = [{x: 1, y: 2}, {x: 3, y: 4}]
+console.log(x1 + " " + y1 + " " + x2 + " " + y2)*/
+
+/*const persona = {nombre: 'Luis', apellidos: 'Molina', telefono: '+34666554433,', dni: '261231823X' , sexo: 'hombre'}
+
+for (const key in persona) {
+    if (Object.prototype.hasOwnProperty.call(persona, key)) {
+        console.log( key + ": " + persona[key]);
+        
+    }
+}*/
+
